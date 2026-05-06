b64dff85feee629151e41515dd794a9fe46ca331
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-05-07 02:12:14 +0300

    Create main.py

---
+++ bot_project/bot/main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from bot.loader import bot, dp
+from bot.db.database import init_db
+
+from bot.handlers import start, link, reply, admin
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+
+    await init_db()
+
+    dp.include_router(start.router)
+    dp.include_router(link.router)
+    dp.include_router(reply.router)
+    dp.include_router(admin.router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
