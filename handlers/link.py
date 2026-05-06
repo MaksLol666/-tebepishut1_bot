55067dc48f35fbccc4959b3175347bc6cbd03601
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-05-07 01:58:16 +0300

    Create link.py

---
+++ handlers/link.py
@@ -0,0 +1,15 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from db.database import update_user
+from config import BOT_USERNAME
+
+router = Router()
+
+
+@router.message(F.text == "📩 Моя ссылка")
+async def link(message: Message):
+    await update_user(message.from_user.id)
+
+    url = f"https://t.me/{BOT_USERNAME}?start={message.from_user.id}"
+
+    await message.answer(f"🔗 Твоя ссылка:\n{url}")
