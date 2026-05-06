b848bb4cf8730b039a65e580c8b9cb39d12147ac
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-05-07 02:51:27 +0300

    Update start.py

--- bot_project/bot/handlers/start.py
+++ bot_project/bot/handlers/start.py
@@ -3,16 +3,27 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from bot.db.database import update_user
-from bot.keyboards.menu import menu
 
 router = Router()
 
 
-@router.message(CommandStart())
-async def start(message: Message):
+@router.message(CommandStart(deep_link=True))
+async def start_with_link(message: Message):
     await update_user(message.from_user.id)
 
+    args = message.text.split()
+
+    # если пришли по ссылке
+    if len(args) > 1:
+        target_id = args[1]
+
+        message.bot_data = {"target": target_id}
+
+        await message.answer(
+            "✍️ Напиши сообщение, которое хочешь отправить анонимно"
+        )
+        return
+
     await message.answer(
-        "🤫 Тебе пишут\n\nВыбери действие 👇",
-        reply_markup=menu
+        "🤫 Тебе пишут\n\nОтправь кому-нибудь свою ссылку 👇"
     )
