732a167fcd32b897631bd51cba7637879596411f
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-05-07 02:55:19 +0300

    Update main.py

--- bot_project/bot/main.py
+++ bot_project/bot/main.py
@@ -4,14 +4,7 @@ import logging
 from bot.loader import bot, dp
 from bot.db.database import init_db
 
-from bot.handlers import (
-    start,
-    link,
-    reply,
-    admin,
-    menu,
-    fallback
-)
+from bot.handlers import start, link, send, inbox
 
 
 async def main():
@@ -21,12 +14,8 @@ async def main():
 
     dp.include_router(start.router)
     dp.include_router(link.router)
-    dp.include_router(reply.router)
-    dp.include_router(admin.router)
-
-    # 🔥 ДОБАВЛЕНО:
-    dp.include_router(menu.router)
-    dp.include_router(fallback.router)
+    dp.include_router(send.router)
+    dp.include_router(inbox.router)
 
     await dp.start_polling(bot)
 
