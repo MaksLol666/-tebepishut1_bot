ffea03e9fc27034c06a5fb7367dc09ce5ef9cab4
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-05-07 02:07:50 +0300

    Delete handlers directory

--- handlers/messages.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, F
-from aiogram.types import Message
-
-router = Router()
-
-
-@router.message(F.text == "📬 Мои сообщения")
-async def my_messages(message: Message):
-    await message.answer("📬 Тут будут твои сообщения")
