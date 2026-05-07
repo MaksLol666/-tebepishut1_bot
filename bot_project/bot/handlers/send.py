6c29a315254f74903a42d0b226cd426983627aa2
Michael <145378101+MaksLol666@users.noreply.github.com>
2026-05-08 00:58:34 +0300

    Update send.py

--- bot_project/bot/handlers/send.py
+++ bot_project/bot/handlers/send.py
@@ -1,27 +1,37 @@
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-from bot.db.database import update_user
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from states.reply import SendMessage
 
 router = Router()
 
 
-@router.message(F.text)
-async def send_anonymous(message: Message, state: FSMContext):
+@router.message(SendMessage.waiting_for_message)
+async def process_message(message: Message, state: FSMContext):
 
     data = await state.get_data()
-    target = data.get("target")
 
-    if not target:
-        return
+    target_id = int(data["target_id"])
 
-    await update_user(message.from_user.id)
-
-    await message.bot.send_message(
-        target,
-        f"💌 Анонимное сообщение:\n\n{message.text}"
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="💬 Ответить",
+                    callback_data=f"reply_{message.from_user.id}"
+                )
+            ]
+        ]
     )
 
-    await message.answer("✅ Отправлено анонимно")
+    await message.bot.send_message(
+        target_id,
+        f"💌 Новое анонимное сообщение:\n\n{message.text}",
+        reply_markup=keyboard
+    )
+
+    await message.answer("✅ Сообщение отправлено")
 
     await state.clear()
